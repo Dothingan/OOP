d09ffd8d8866636fef97a8167f690219b2e4ba77
Lương Duy Tân <121930696+LuongDuyTan@users.noreply.github.com>
2024-02-29 16:56:45 +0700

    Create Switch.java

---
+++ BaiTapChung/Week1/Switch.java
@@ -0,0 +1,19 @@
+public class Switch{
+    public static void main(String[] args) {
+        int menu = 4;
+        switch (menu) {
+            case 1:
+            System.out.println("Ham");
+            break;
+            case 2:
+            System.out.println("Noodle");
+            break;
+            case 3:
+            System.out.println("Pizza");
+            break;
+            case 4:
+            System.out.println("Fries");
+            break;
+        }
+    }
+}

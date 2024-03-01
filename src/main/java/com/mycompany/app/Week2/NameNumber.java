cd170c06f7aa7c1eced70d7f602df7f98f8535cd
Dothingan <121930750+Dothingan@users.noreply.github.com>
2024-03-01 02:17:26 +0000

    update

---
+++ src/main/java/com/mycompany/app/Week2/NameNumber.java
@@ -0,0 +1,16 @@
+package com.mycompany.app.Week2;
+
+public class NameNumber {
+    private String lastName;
+    private String telNumber;
+    public void setNameNumber(String name, String num){
+        lastName = name;
+        telNumber = num;
+    }
+    String getLastName(){
+        return lastName;
+    }
+    String getTelNumber(){
+        return telNumber;
+    }
+}
---
+++ src/main/java/com/mycompany/app/Week2/Recursion.java
@@ -0,0 +1,20 @@
+package com.mycompany.app.Week2;
+
+public class Recursion {
+    int num;
+
+    Recursion(){};
+
+    //nhan 1 so nguyen
+    Recursion(int number){};
+
+    //tra ve ket qua de quy
+    int Recursion(int number){
+        this.num = number;
+        if(this.num <= 1){
+            return 1;
+        }
+        else
+            return this.num*Recursion(this.num - 1);
+    }
+}

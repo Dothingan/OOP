a0f1a5532a7873e3bd72e2ee4372bd8c9d8aadf1
Dothingan <121930750+Dothingan@users.noreply.github.com>
2024-03-08 02:16:41 +0000

    up

---
+++ src/main/java/com/mycompany/app/Week3/code3/Abstract/Animal.java
@@ -0,0 +1,8 @@
+package com.mycompany.app.Week3.code3.Abstract;
+
+public abstract class Animal {
+    public abstract void animalSound();
+    public void sleep(){
+        System.out.println("Zzz");
+    }
+}

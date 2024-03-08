fe1e5d0b7909dc9ec751231a5e881e3b5a9d8183
Dothingan <121930750+Dothingan@users.noreply.github.com>
2024-03-08 02:03:18 +0000

    up

---
+++ src/main/java/com/mycompany/app/Week3/code3/Polymorphimsm/Pig.java
@@ -0,0 +1,7 @@
+package com.mycompany.app.Week3.code3.Polymorphimsm;
+
+public class Pig extends Animal{
+    public void animalSound(){
+        System.out.println("The pig says: wee wee");
+    }
+}

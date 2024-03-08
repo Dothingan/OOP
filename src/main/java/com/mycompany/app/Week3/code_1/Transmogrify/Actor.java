8cbfd2c0d5cd3f15a487b051c4e47d9cbfba289e
Dothingan <121930750+Dothingan@users.noreply.github.com>
2024-03-08 01:26:41 +0000

    up

---
+++ src/main/java/com/mycompany/app/Week3/code_1/Transmogrify/Actor.java
@@ -0,0 +1,5 @@
+package com.mycompany.app.Week3.code_1.Transmogrify;
+
+public abstract class Actor {
+    abstract void act();
+}
---
+++ src/main/java/com/mycompany/app/Week3/code_1/Transmogrify/HappyActor.java
@@ -0,0 +1,7 @@
+package com.mycompany.app.Week3.code_1.Transmogrify;
+
+public class HappyActor extends Actor {
+    public void act(){
+        System.out.println("しあわせ");
+    }
+}
